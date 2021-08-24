35aab12b437a983000a12d1d80cf66733dd4b0bd
Thibaut <tybo.orcel@gmail.com>
2021-08-24 20:44:15 +0200

    first commit

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-// hellow world!
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+
+app.use(express.json());
+app.use(cors());
+
+
+
+app.listen(5500, () => {
+    console.log('Backend server is running!');
+});
